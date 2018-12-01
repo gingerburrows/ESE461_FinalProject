638dcd165f4ce9941a6030ada1af8e96e8628c7f
lyqMelon <44246370+lyqMelon@users.noreply.github.com>
2018-12-01 10:34:59 -0600

    Updated RAM partition

--- SRAM/ram_template.v
+++ SRAM/ram_template.v
@@ -1,58 +1,71 @@
+module template();
 reg [15:0] wrData;
-reg in_we [0:63];
-reg [3:0]inputAddrQ,inputAddrD;
-wire [15:0] inputData [0:63];
+reg [63:0] we [0:1];
+reg [3:0]inoutAddrQ,inoutAddrD;
+wire [15:0] inoutData [0:63];
 genvar i;
-
-//Input SRAM Size: 16*12+48*12 = 784
+wire clk;
+//Input/Output SRAM Size: 16*12+48*12 = 784
 generate
-	for(i = 0; i < 64; i = i + 1) begin
-		sram #(.SIZE(i < 16 ? 13:12)) input_ram(
+	for(i = 0; i < 64; i = i + 1) begin : InOutSram
+		sram #(.ADDR_WIDTH(4),.SIZE(i < 16 ? 13:12)) input_ram(
 			.clk(clk),
-			.we(we[i]),
-			.addr(inputAddrQ),
+			.we(we[1][i]),
+			.addr(inoutAddrQ),
 			.data(wrData),
-			.q(inputData[i]),
+			.q(inoutData[i])
 	);
 	end
 endgenerate
 
 
-reg[17:0] wAddrD,wAddrQ;
-wire [15:0] weight [0:1]
+reg[12:0] wAddrD,wAddrQ;
+wire [15:0] weight0 [0:63]; //Output for first stage
+wire [15:0] weight1 [0:63]; //Output for second stage
+
 //Uncomment the line below if write is needed to weight SRAM
 //reg w_we [0:1] //0-1st Round Weight  1- 2nd Round Weight
 
-//Weight SRAM (Weight Reuse)
-sram #(.ADDR_WIDTH(18),.SIZE(156800)) weight1(
-		.clk(clk),
-		.we(0),//Okay to tie to ground?
-		.addr(wAddrQ),
-		.data(0), //Okay to tie to ground?
-		.q(weight[0]), 
-	);
-	
-sram #(.ADDR_WIDTH(11),.SIZE(2000)) weight2(
-		.clk(clk),
-		.we(0),//Okay to tie to ground?
-		.addr(wAddrQ[10:0]),
-		.data(0), //Okay to tie to ground?
-		.q(weight[1]), 
-	);
-
-reg out_we [0:63];
-reg [4:0] outAddrQ,outAddrD;
-wire [15:0] outData [0:63];
-
-//Output SRAM (Hold Intermediate Result) Size: 16*32+48*31 = 2000
+//Weight SRAM 1 Size: 16*13*200+48*12*200 = 156800
 generate
-	for(i = 0; i < 64; i = i + 1) begin
-		sram #(.ADDR_WIDTH(5),.SIZE(i < 16 ? 32:31)) output_ram(
+	for(i = 0;i < 64;i=i+1) begin : w1
+		sram #(.ADDR_WIDTH(12),.SIZE(i<16 ? 2600 : 2400)) weight_sram1(
+				.clk(clk),
+				.we(0),//Okay to tie to ground?
+				.addr(wAddrQ),
+				.data(0), //Okay to tie to ground?
+				.q(weight0[i])
+			);
+	end
+endgenerate
+
+//Weight SRAM 2 Size: 8*4*10+56*3*10= 2000
+generate
+	for(i = 0;i < 64;i=i+1) begin :w2
+		sram #(.ADDR_WIDTH(6),.SIZE(i <8 ? 40:30)) weight_sram2(
+				.clk(clk),
+				.we(0),//Okay to tie to ground?
+				.addr(wAddrQ[5:0]),
+				.data(0), //Okay to tie to ground?
+				.q(weight1[1])
+			);
+	end
+endgenerate
+
+reg mid_we [0:63];
+reg [4:0] midAddrQ,midAddrD;
+wire [15:0] midData [0:63];
+
+//Intermediate SRAM (Hold Intermediate Result) Size: 8*4+56*3 = 200
+generate
+	for(i = 0; i < 64; i = i + 1) begin :tempSram
+		sram #(.ADDR_WIDTH(2),.SIZE(i < 8 ? 4:3)) output_ram(
 			.clk(clk),
-			.we(out_we[i]),
-			.addr(outAddrQ),
+			.we(we[0][i]),
+			.addr(midAddrQ),
 			.data(wrData),
-			.q(outData[i]),
+			.q(midData[i])
 	);
 	end
 endgenerate
+endmodule
