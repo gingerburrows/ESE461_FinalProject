b56e5cecd423d3f9cec75ef9a172c15557a98465
lyqMelon <44246370+lyqMelon@users.noreply.github.com>
2018-12-01 10:34:30 -0600

    Error Correction

--- SRAM/sram.v
+++ SRAM/sram.v
@@ -2,7 +2,7 @@ module sram #(parameter ADDR_WIDTH = 4,DATA_WIDTH = 16,SIZE=(1<<ADDR_WIDTH))(
 	input clk,we,
 	input [ADDR_WIDTH-1:0] addr,
 	input [DATA_WIDTH-1:0] data,
-	output [DATA_WIDTH-1:0] q,
+	output [DATA_WIDTH-1:0] q
 	);
 	
 	reg [DATA_WIDTH-1:0] ram [0:SIZE-1];
