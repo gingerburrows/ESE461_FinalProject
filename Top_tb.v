d3cacb058274ceb2997e4f9cd92da0c64a71b803
lyqMelon <44246370+lyqMelon@users.noreply.github.com>
2018-12-04 21:21:02 -0600

    Newest Revision (FIXME)

--- Top_tb.v
+++ Top_tb.v
@@ -4,12 +4,12 @@ module top_tb();
 	localparam CLK_PERIOD = 100;
 	localparam testImage = "test_image1_fix.txt";
 	reg clk,rst;
-	integer i,j,k;
+	genvar i,j,k;
 	
 	Top DUT(
-		.CLK(clk),
-		.Reset(rst)
-	};
+		.clk(clk),
+		.reset(rst)
+	);
 	
 	//Generate clock
 	always begin
@@ -21,6 +21,45 @@ module top_tb();
 	reg [15:0] weight_1 [0:15799];
 	reg [15:0] weight_2 [0:1999];
 
+	generate
+		//Initialize Input SRAM
+		for(i = 0; i < 64;i=i+1) begin : inputNum
+			for(j = 0; j < (i < 16 ? 13:12);j = j + 1) begin :inputAddr
+				DUT.InOutSram[i].ram.ram[j] = image_in[64*j+i];
+			end
+		end
+		
+		//Initialize weight SRAMs
+		
+		//Weight Matrix 1
+		for(k = 0; k < 200;k = k +1) begin :weight1Val
+			for(i = 0; i <64;i=i+1) begin:weight1Num
+				for(j = 0;j<(i<16 ? 13 : 12);j = j+1) begin :weight1Addr
+					if(i < 16) begin
+						DUT.w1[i].wram1.ram[13*k+j] = weight_1[64*j+i+784*k];
+					end else begin
+						DUT.w1[i].wram1.ram[12*k+j] = weight_1[64*j+i+784*k];
+					end
+				end
+			end
+		end
+		
+		//Weight Matrix 2
+		for(k = 0; k < 10;k = k +1) begin : weight2Val
+			for(i = 0; i <64;i=i+1) begin : weight2Num
+				for(j = 0;j<(i<8 ? 4 : 3);j = j+1) begin : weight2Addr
+					if(i < 8) begin
+						DUT.w2[i].wram2.ram[4*k+j] = weight_1[64*j+i+200*k];
+					end else begin
+						DUT.w2[i].wram2.ram[3*k+j] = weight_1[64*j+i+200*k];
+					end
+				end
+			end
+		end
+		
+		endgenerate	
+
+
 	initial begin
 		$dumpfile("top.vcd");
         $dumpvars(0, top_tb);
@@ -28,42 +67,6 @@ module top_tb();
 		$readmemb(testImage,image_in);
 		$readmemb("layer1_weight.txt",weight_1);
 		$readmemb("layer2_weight.txt",weight_2);
-		
-		//Initialize Input SRAM
-		for(i = 0; i < 64;i=i+1) begin
-			for(j = 0; j < (i < 16 ? 13:12);j = j + 1) begin
-				DUT.InOutSram[i].ram[j] = image_in[64*j+i];
-			end
-		end
-		
-		//Initialize weight SRAMs
-		
-		//Weight Matrix 1
-		for(k = 0; k < 200;k = k +1) begin
-			for(i = 0; i <64;i=i+1) begin
-				for(j = 0;j<(i<16 ? 13 : 12);j = j+1) begin
-					if(i < 16) begin
-						DUT.w1[i].ram[13*k+j] = weight_1[64*j+i+784*k];
-					end else begin
-						DUT.w1[i].ram[12*k+j] = weight_1[64*j+i+784*k];
-					end
-				end
-			end
-		end
-		
-		//Weight Matrix 2
-		for(k = 0; k < 10;k = k +1) begin
-			for(i = 0; i <64;i=i+1) begin
-				for(j = 0;j<(i<8 ? 4 : 3);j = j+1) begin
-					if(i < 8) begin
-						DUT.w2[i].ram[4*k+j] = weight_1[64*j+i+200*k];
-					end else begin
-						DUT.w2[i].ram[3*k+j] = weight_1[64*j+i+200*k];
-					end
-				end
-			end
-		end
-		
 		
 		rst = 1;
 		clk = 0;
