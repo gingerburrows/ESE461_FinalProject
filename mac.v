4e8da414c0da5a7651b40ea26b48cbdcb9a4a590
gingerburrows <40743226+gingerburrows@users.noreply.github.com>
2018-12-04 22:43:12 -0600

    Update mac.v

--- mac.v
+++ mac.v
@@ -1,6 +1,7 @@
 module mac
 (
 	input clk,
+	input reset,
 	input [15:0] data1,
 	input [15:0] data2,
 	input [15:0] data3,
@@ -65,143 +66,163 @@ module mac
 	input [15:0] data62,
 	input [15:0] data63,
 	input [15:0] data64,
-	input [15:0] weight,
-	output reg [15:0] out1,
-	output reg [15:0] out2,
-	output reg [15:0] out3,
-	output reg [15:0] out4,
-	output reg [15:0] out5,
-	output reg [15:0] out6,
-	output reg [15:0] out7,
-	output reg [15:0] out8,
-	output reg [15:0] out9,
-	output reg [15:0] out10,
-	output reg [15:0] out11,
-	output reg [15:0] out12,
-	output reg [15:0] out13,
-	output reg [15:0] out14,
-	output reg [15:0] out15,
-	output reg [15:0] out16,
-	output reg [15:0] out17,
-	output reg [15:0] out18,
-	output reg [15:0] out19,
-	output reg [15:0] out20,
-	output reg [15:0] out21,
-	output reg [15:0] out22,
-	output reg [15:0] out23,
-	output reg [15:0] out24,
-	output reg [15:0] out25,
-	output reg [15:0] out26,
-	output reg [15:0] out27,
-	output reg [15:0] out28,
-	output reg [15:0] out29,
-	output reg [15:0] out30,
-	output reg [15:0] out31,
-	output reg [15:0] out32,
-	output reg [15:0] out33,
-	output reg [15:0] out34,
-	output reg [15:0] out35,
-	output reg [15:0] out36,
-	output reg [15:0] out37,
-	output reg [15:0] out38,
-	output reg [15:0] out39,
-	output reg [15:0] out40,
-	output reg [15:0] out41,
-	output reg [15:0] out42,
-	output reg [15:0] out43,
-	output reg [15:0] out44,
-	output reg [15:0] out45,
-	output reg [15:0] out46,
-	output reg [15:0] out47,
-	output reg [15:0] out48,
-	output reg [15:0] out49,
-	output reg [15:0] out50,
-	output reg [15:0] out51,
-	output reg [15:0] out52,
-	output reg [15:0] out53,
-	output reg [15:0] out54,
-	output reg [15:0] out55,
-	output reg [15:0] out56,
-	output reg [15:0] out57,
-	output reg [15:0] out58,
-	output reg [15:0] out59,
-	output reg [15:0] out60,
-	output reg [15:0] out61,
-	output reg [15:0] out62,
-	output reg [15:0] out63,
-	output reg [15:0] out64
+	input [15:0] weight1,
+	input [15:0] weight2,
+	input [15:0] weight3,
+	input [15:0] weight4,
+	input [15:0] weight5,
+	input [15:0] weight6,
+	input [15:0] weight7,
+	input [15:0] weight8,
+	input [15:0] weight9,
+	input [15:0] weight10,
+	input [15:0] weight11,
+	input [15:0] weight12,
+	input [15:0] weight13,
+	input [15:0] weight14,
+	input [15:0] weight15,
+	input [15:0] weight16,
+	input [15:0] weight17,
+	input [15:0] weight18,
+	input [15:0] weight19,
+	input [15:0] weight20,
+	input [15:0] weight21,
+	input [15:0] weight22,
+	input [15:0] weight23,
+	input [15:0] weight24,
+	input [15:0] weight25,
+	input [15:0] weight26,
+	input [15:0] weight27,
+	input [15:0] weight28,
+	input [15:0] weight29,
+	input [15:0] weight30,
+	input [15:0] weight31,
+	input [15:0] weight32,
+	input [15:0] weight33,
+	input [15:0] weight34,
+	input [15:0] weight35,
+	input [15:0] weight36,
+	input [15:0] weight37,
+	input [15:0] weight38,
+	input [15:0] weight39,
+	input [15:0] weight40,
+	input [15:0] weight41,
+	input [15:0] weight42,
+	input [15:0] weight43,
+	input [15:0] weight44,
+	input [15:0] weight45,
+	input [15:0] weight46,
+	input [15:0] weight47,
+	input [15:0] weight48,
+	input [15:0] weight49,
+	input [15:0] weight50,
+	input [15:0] weight51,
+	input [15:0] weight52,
+	input [15:0] weight53,
+	input [15:0] weight54,
+	input [15:0] weight55,
+	input [15:0] weight56,
+	input [15:0] weight57,
+	input [15:0] weight58,
+	input [15:0] weight59,
+	input [15:0] weight60,
+	input [15:0] weight61,
+	input [15:0] weight62,
+	input [15:0] weight63,
+	input [15:0] weight64,
+	output reg [15:0] result
 );
 
-	function [15:0] trunc_32_to_16(input [31:0] val32);
-  		trunc_32_to_16 = val32[23:8];
-	endfunction
+	reg [31 : 0] psums[63 : 0];
+	reg [31 : 0] result_temp;
+	reg i;
 
-	always@(posedge clk) begin
-		out1 <= trunc_32_to_16(data1*weight);
-		out2 <= trunc_32_to_16(data2*weight);
-		out3 <= trunc_32_to_16(data3*weight);
-		out4 <= trunc_32_to_16(data4*weight);
-		out5 <= trunc_32_to_16(data5*weight);
-		out6 <= trunc_32_to_16(data6*weight);
-		out7 <= trunc_32_to_16(data7*weight);
-		out8 <= trunc_32_to_16(data8*weight);
-		out9 <= trunc_32_to_16(data9*weight);
-		out10 <= trunc_32_to_16(data10*weight);
-		out11 <= trunc_32_to_16(data11*weight);
-		out12 <= trunc_32_to_16(data12*weight);
-		out13 <= trunc_32_to_16(data13*weight);
-		out14 <= trunc_32_to_16(data14*weight);
-		out15 <= trunc_32_to_16(data15*weight);
-		out16 <= trunc_32_to_16(data16*weight);
-		out17 <= trunc_32_to_16(data17*weight);
-		out18 <= trunc_32_to_16(data18*weight);
-		out19 <= trunc_32_to_16(data19*weight);
-		out20 <= trunc_32_to_16(data20*weight);
-		out21 <= trunc_32_to_16(data21*weight);
-		out22 <= trunc_32_to_16(data22*weight);
-		out23 <= trunc_32_to_16(data23*weight);
-		out24 <= trunc_32_to_16(data24*weight);
-		out25 <= trunc_32_to_16(data25*weight);
-		out26 <= trunc_32_to_16(data26*weight);
-		out27 <= trunc_32_to_16(data27*weight);
-		out28 <= trunc_32_to_16(data28*weight);
-		out29 <= trunc_32_to_16(data29*weight);
-		out30 <= trunc_32_to_16(data30*weight);
-		out31 <= trunc_32_to_16(data31*weight);
-		out32 <= trunc_32_to_16(data32*weight);
-		out33 <= trunc_32_to_16(data33*weight);
-		out34 <= trunc_32_to_16(data34*weight);
-		out35 <= trunc_32_to_16(data35*weight);
-		out36 <= trunc_32_to_16(data36*weight);
-		out37 <= trunc_32_to_16(data37*weight);
-		out38 <= trunc_32_to_16(data38*weight);
-		out39 <= trunc_32_to_16(data39*weight);
-		out40 <= trunc_32_to_16(data40*weight);
-		out41 <= trunc_32_to_16(data41*weight);
-		out42 <= trunc_32_to_16(data42*weight);
-		out43 <= trunc_32_to_16(data43*weight);
-		out44 <= trunc_32_to_16(data44*weight);
-		out45 <= trunc_32_to_16(data45*weight);
-		out46 <= trunc_32_to_16(data46*weight);
-		out47 <= trunc_32_to_16(data47*weight);
-		out48 <= trunc_32_to_16(data48*weight);
-		out49 <= trunc_32_to_16(data49*weight);
-		out50 <= trunc_32_to_16(data50*weight);
-		out51 <= trunc_32_to_16(data51*weight);
-		out52 <= trunc_32_to_16(data52*weight);
-		out53 <= trunc_32_to_16(data53*weight);
-		out54 <= trunc_32_to_16(data54*weight);
-		out55 <= trunc_32_to_16(data55*weight);
-		out56 <= trunc_32_to_16(data56*weight);
-		out57 <= trunc_32_to_16(data57*weight);
-		out58 <= trunc_32_to_16(data58*weight);
-		out59 <= trunc_32_to_16(data59*weight);
-		out60 <= trunc_32_to_16(data60*weight);
-		out61 <= trunc_32_to_16(data61*weight);
-		out62 <= trunc_32_to_16(data62*weight);
-		out63 <= trunc_32_to_16(data63*weight);
-		out64 <= trunc_32_to_16(data64*weight);
+	//function [15:0] trunc_32_to_16(input [31:0] val32);
+  		//trunc_32_to_16 = val32[23:8];
+	//endfunction
+
+	always@(posedge clk or posedge reset) begin
+		if (reset) begin
+			for(i = 0; i < 64; i = i + 1) begin
+				psums[i] <= 0;
+			end
+			result_temp <= 0;
+			result <= 0;
+		end	
+		else begin
+			psums[0] <= data1 * weight1;
+			psums[1] <= data2 * weight2;
+			psums[2] <= data3 * weight3;
+			psums[3] <= data4 * weight4;
+			psums[4] <= data5 * weight5;
+			psums[5] <= data6 * weight6;
+			psums[6] <= data7 * weight7;
+			psums[7] <= data8 * weight8;
+			psums[8] <= data9 * weight9;
+			psums[9] <= data10 * weight10;
+			psums[10] <= data11 * weight11;
+			psums[11] <= data12 * weight12;
+			psums[12] <= data13 * weight13;
+			psums[13] <= data14 * weight14;
+			psums[14] <= data15 * weight15;
+			psums[15] <= data16 * weight16;
+			psums[16] <= data17 * weight17;
+			psums[17] <= data18 * weight18;
+			psums[18] <= data19 * weight19;
+			psums[19] <= data20 * weight20;
+			psums[20] <= data21 * weight21;
+			psums[21] <= data22 * weight22;
+			psums[22] <= data23 * weight23;
+			psums[23] <= data24 * weight24;
+			psums[24] <= data25 * weight25;
+			psums[25] <= data26 * weight26;
+			psums[26] <= data27 * weight27;
+			psums[27] <= data28 * weight28;
+			psums[28] <= data29 * weight29;
+			psums[29] <= data30 * weight30;
+			psums[30] <= data31 * weight31;
+			psums[31] <= data32 * weight32;
+			psums[32] <= data33 * weight33;
+			psums[33] <= data34 * weight34;
+			psums[34] <= data35 * weight35;
+			psums[35] <= data36 * weight36;
+			psums[36] <= data37 * weight37;
+			psums[37] <= data38 * weight38;
+			psums[38] <= data39 * weight39;
+			psums[39] <= data40 * weight40;
+			psums[40] <= data41 * weight41;
+			psums[41] <= data42 * weight42;
+			psums[42] <= data43 * weight43;
+			psums[43] <= data44 * weight44;
+			psums[44] <= data45 * weight45;
+			psums[45] <= data46 * weight46;
+			psums[46] <= data47 * weight47;
+			psums[47] <= data48 * weight48;
+			psums[48] <= data49 * weight49;
+			psums[49] <= data50 * weight50;
+			psums[50] <= data51 * weight51;
+			psums[51] <= data52 * weight52;
+			psums[52] <= data53 * weight53;
+			psums[53] <= data54 * weight54;
+			psums[54] <= data55 * weight55;
+			psums[55] <= data56 * weight56;
+			psums[56] <= data57 * weight57;
+			psums[57] <= data58 * weight58;
+			psums[58] <= data59 * weight59;
+			psums[59] <= data60 * weight60;
+			psums[60] <= data61 * weight61;
+			psums[61] <= data62 * weight62;
+			psums[62] <= data63 * weight63;
+			psums[63] <= data64 * weight64;
+		
+			for(i = 0; i < 64; i = i + 1) begin
+				result_temp <=  result_temp + psums[i];
+			end
+
+			result <= result_temp[23:8];
+		end
 	end
+
 endmodule
 
 
